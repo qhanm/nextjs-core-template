4c47030294b328a91252f9370cf6407b733d24c4
nam@mtms.live <nam@mtms.live>
2024-05-25 20:29:22 +0700

    update

--- src/components/text-input/index.tsx
+++ src/components/text-input/index.tsx
@@ -1,16 +1,27 @@
 import { TextField, TextFieldProps, styled } from '@mui/material';
 
-const TextFieldStyled = styled(TextField)<TextFieldProps>(({ theme }) => {
+const TextFieldStyled = styled(TextField)<TextFieldProps>((props) => {
   return {
     '& .MuiFormHelperText-root': {
       marginLeft: 0,
       marginRight: 0,
     },
+    '& .MuiInputBase-root input': {
+      padding: props.size === 'medium' ? '14px 10px' : '8px 14px',
+    },
+    '& .MuiInputLabel-root': {
+      transform: 'none',
+      position: 'relative',
+      marginBottom: '4px',
+    },
+    '& fieldset legend': {
+      width: 0,
+    },
   };
 });
 
-const TextInput = (props: TextFieldProps) => {
-  return <TextFieldStyled {...props} />;
+const TextInput = ({ size = 'medium', ...restProps }: TextFieldProps) => {
+  return <TextFieldStyled {...restProps} size={size} focused />;
 };
 
 export default TextInput;
